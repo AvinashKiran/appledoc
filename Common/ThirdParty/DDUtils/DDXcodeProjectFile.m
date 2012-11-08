df5213e9ae07eea9e8fa33ac0c9b5f1e8520c4b3
Govind <govindaraomca@gmail.com>
2012-11-08 16:31:29 +0530

    test

--- Common/ThirdParty/DDUtils/DDXcodeProjectFile.m
+++ Common/ThirdParty/DDUtils/DDXcodeProjectFile.m
@@ -92,87 +92,89 @@
 #pragma mark - main parse
 
 - (BOOL)parse:(NSError**)pError {
-    NSAssert(self.path.length || self.dictionary.count, @"we should have a non-empty path or non-empty dictionary");
     
-    //get initial dictionary
-    if(self.path.length) {
-        self.dictionary = [NSDictionary dictionaryWithContentsOfFile:self.path];
-        if(!self.dictionary) {
-            if(pError)
-                *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot make plist from file contents"}];
-            return NO;
-        }
-    }
-    if(!self.dictionary) {
-        if(pError)
-            *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot make plist from file contents"}];
-        return NO;
-    }
-    
-    //get main objects dictionary
-    NSDictionary *objects = self.dictionary[@"objects"];
-    if(![objects isKindOfClass:[NSDictionary class]]) {
-        if(pError)
-            *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot find main objects dictionary"}];
-        return NO;
-    }
-    
-    //set up _mutableFilesBuffer which will get filled in the parsing process.. or not :D
-    _mutableFilesBuffer = [NSMutableArray arrayWithCapacity:objects.count/2];
-    
-    //handle each object that can appear in a method found via string to SEL
-    for (NSDictionary *object in objects.allValues) {
-        if(![object isKindOfClass:[NSDictionary class]] || !object[@"isa"]) {
-            if(pError)
-                *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"cannot handle object %@", object]}];
-            return NO;
-        }
-            
-        //get method by isa
-        SEL method = [self methodNameForIsa:object[@"isa"]];
-        if(!method) {
-            if(pError)
-                *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"cannot handle isa %@. Selector: %@", object[@"isa"], NSStringFromSelector(method)]}];
-            return NO;
-        }
-        
-        //if it doesnt respond to the method, log it and continue for now
-        if(![self respondsToSelector:method]) {
-            NSLog(@"Warn: dont handle %@", NSStringFromSelector(method));
-            continue;
-        }
-        
-        //call the reflected method
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        NSError *suberror = [self performSelector:method withObject:object];
-#pragma clang diagnostic pop
-
-        //exit on error
-        if(suberror) {
-            if(pError)
-                *pError = suberror;
-            return NO;
-        }
-    }
-    
-    //check mandatory... 1 file at least
-    if( !_mutableFilesBuffer.count ) {
-        if(pError)
-            *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot find manatory attributes in plist from file contents"}];
-        return NO;
-    }
-    
-    //resolve all
-    NSError *suberror = [self resolveFiles:_mutableFilesBuffer];
-    if(suberror) {
-        if(pError)
-            *pError = suberror;
-        return NO;
-    }
-    
-    //save buffer to property
-    self.files = [NSArray arrayWithArray:_mutableFilesBuffer];
+    NSLog(@"Error: %@",pError);
+//    NSAssert(self.path.length || self.dictionary.count, @"we should have a non-empty path or non-empty dictionary");
+//    
+//    //get initial dictionary
+//    if(self.path.length) {
+//        self.dictionary = [NSDictionary dictionaryWithContentsOfFile:self.path];
+//        if(!self.dictionary) {
+//            if(pError)
+//                *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot make plist from file contents"}];
+//            return NO;
+//        }
+//    }
+//    if(!self.dictionary) {
+//        if(pError)
+//            *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot make plist from file contents"}];
+//        return NO;
+//    }
+//    
+//    //get main objects dictionary
+//    NSDictionary *objects = self.dictionary[@"objects"];
+//    if(![objects isKindOfClass:[NSDictionary class]]) {
+//        if(pError)
+//            *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot find main objects dictionary"}];
+//        return NO;
+//    }
+//    
+//    //set up _mutableFilesBuffer which will get filled in the parsing process.. or not :D
+//    _mutableFilesBuffer = [NSMutableArray arrayWithCapacity:objects.count/2];
+//    
+//    //handle each object that can appear in a method found via string to SEL
+//    for (NSDictionary *object in objects.allValues) {
+//        if(![object isKindOfClass:[NSDictionary class]] || !object[@"isa"]) {
+//            if(pError)
+//                *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"cannot handle object %@", object]}];
+//            return NO;
+//        }
+//            
+//        //get method by isa
+//        SEL method = [self methodNameForIsa:object[@"isa"]];
+//        if(!method) {
+//            if(pError)
+//                *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"cannot handle isa %@. Selector: %@", object[@"isa"], NSStringFromSelector(method)]}];
+//            return NO;
+//        }
+//        
+//        //if it doesnt respond to the method, log it and continue for now
+//        if(![self respondsToSelector:method]) {
+//            NSLog(@"Warn: dont handle %@", NSStringFromSelector(method));
+//            continue;
+//        }
+//        
+//        //call the reflected method
+//#pragma clang diagnostic push
+//#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+//        NSError *suberror = [self performSelector:method withObject:object];
+//#pragma clang diagnostic pop
+//
+//        //exit on error
+//        if(suberror) {
+//            if(pError)
+//                *pError = suberror;
+//            return NO;
+//        }
+//    }
+//    
+//    //check mandatory... 1 file at least
+//    if( !_mutableFilesBuffer.count ) {
+//        if(pError)
+//            *pError = [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:@"cannot find manatory attributes in plist from file contents"}];
+//        return NO;
+//    }
+//    
+//    //resolve all
+//    NSError *suberror = [self resolveFiles:_mutableFilesBuffer];
+//    if(suberror) {
+//        if(pError)
+//            *pError = suberror;
+//        return NO;
+//    }
+//    
+//    //save buffer to property
+//    self.files = [NSArray arrayWithArray:_mutableFilesBuffer];
 
     return YES;
 }
@@ -199,42 +201,43 @@
 // we only care about those for now
 
 - (NSError*)parsePBXProject:(NSDictionary*)dict {
-    if(!dict[@"attributes"]) {
-        return [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"PBXProject without attributes: %@", dict]}];
-    }
-
-    self.classPrefix = dict[@"attributes"][@"CLASSPREFIX"];
-    self.company = dict[@"attributes"][@"ORGANIZATIONNAME"];
-    self.developmentRegion = dict[@"developmentRegion"];
-    self.minimumVersion = dict[@"compatibilityVersion"];
-
-    if(dict[@"projectRoot"]) {
-        if(self.path.length)
-            self.projectRoot = [self.path.stringByDeletingLastPathComponent.stringByDeletingLastPathComponent stringByAppendingPathComponent:dict[@"projectRoot"]];
-        else
-            self.projectRoot =  dict[@"projectRoot"];
-    }
-    
-    if(!self.company || !self.projectRoot) {
-        return [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"PBXProject missing mandatory attributes: %@", dict]}];
-    }
+    NSLog(@"dict: %@",dict);
+//    if(!dict[@"attributes"]) {
+//        return [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"PBXProject without attributes: %@", dict]}];
+//    }
+//
+//    self.classPrefix = dict[@"attributes"][@"CLASSPREFIX"];
+//    self.company = dict[@"attributes"][@"ORGANIZATIONNAME"];
+//    self.developmentRegion = dict[@"developmentRegion"];
+//    self.minimumVersion = dict[@"compatibilityVersion"];
+//
+//    if(dict[@"projectRoot"]) {
+//        if(self.path.length)
+//            self.projectRoot = [self.path.stringByDeletingLastPathComponent.stringByDeletingLastPathComponent stringByAppendingPathComponent:dict[@"projectRoot"]];
+//        else
+//            self.projectRoot =  dict[@"projectRoot"];
+//    }
+//    
+//    if(!self.company || !self.projectRoot) {
+//        return [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"PBXProject missing mandatory attributes: %@", dict]}];
+//    }
     
     return nil;
 }
 
 - (NSError*)parsePBXFileReference:(NSDictionary*)dict {
-    if(!dict[@"lastKnownFileType"] && dict[@"explicitFileType"]) {
-        NSMutableDictionary *mdict = dict.mutableCopy;
-        mdict[@"lastKnownFileType"] = dict[@"explicitFileType"];
-        dict = mdict;
-    }
-    
-    if(!dict[@"path"] || !dict[@"lastKnownFileType"]) {
-        return [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"PBXFileReference without path/type: %@", dict]}];
-    }
-    
-    //add the path - TO BE resolved later :D
-    [_mutableFilesBuffer addObject:@{@"path": dict[@"path"], @"type": dict[@"lastKnownFileType"]}];
+//    if(!dict[@"lastKnownFileType"] && dict[@"explicitFileType"]) {
+//        NSMutableDictionary *mdict = dict.mutableCopy;
+//        mdict[@"lastKnownFileType"] = dict[@"explicitFileType"];
+//        dict = mdict;
+//    }
+//    
+//    if(!dict[@"path"] || !dict[@"lastKnownFileType"]) {
+//        return [NSError errorWithDomain:@"DDXcodeProjectFile" code:0 userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat:@"PBXFileReference without path/type: %@", dict]}];
+//    }
+//    
+//    //add the path - TO BE resolved later :D
+//    [_mutableFilesBuffer addObject:@{@"path": dict[@"path"], @"type": dict[@"lastKnownFileType"]}];
     
     return nil; //no error
 }
